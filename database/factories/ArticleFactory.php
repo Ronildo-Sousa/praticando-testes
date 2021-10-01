37f061f239a44fb3e2cd645bb328637164411a68
Ronildo Sousa <sousaronildo400@gmail.com>
2021-10-01 10:54:47 -0300

    migrations + seeders

---
+++ database/factories/ArticleFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Article;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ArticleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Article::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence(4),
+            'content' => $this->faker->sentence(30),
+            'num_likes' => random_int(1, 50),
+            'published_at' => now()
+        ];
+    }
+}
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Article;
+use App\Models\Comment;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,6 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create();
+
+        Article::factory()
+            ->has(Comment::factory()->for($user)->count(2))
+            ->count(3)
+            ->for($user)
+            ->create();
     }
 }
